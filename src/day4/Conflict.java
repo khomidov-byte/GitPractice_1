cee898bc2e29fd167cbfa871423b37ade0c6b04a
khomidov-byte <84728718+khomidov-byte@users.noreply.github.com>
2021-07-30 10:50:03 -0400

    master collection is done

--- src/day4/Conflict.java
+++ src/day4/Conflict.java
@@ -5,6 +5,6 @@ public class Conflict {
     // This is master branch content
     // this is collection branch content
     // I can type anything i want here
-    //
+    //this is the change collection2 branch does not know about
 
 }
